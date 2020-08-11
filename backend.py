4bc0997e5cb12ba81e272ce281710861caf15156
Your Name <legendofmiracles@protonmail.com>
2020-08-11 17:15:11 +0200

    now returns a json array of the videos

--- backend.py
+++ backend.py
@@ -1,17 +1,47 @@
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import json
+
 driver = webdriver.Firefox()
 
 
 def request(url):
-    response = driver.get(url)
-    return response
+    driver.get(url)
+    #return response
 
 def get_main_page():
-    html = request('https://www.youtube.com/')
+    titles_array, urls_array,  creators_array = [], [], []
+    request('https://www.youtube.com/')
+    
+    # get titles
     titles = driver.find_elements_by_id("video-title")
-    print(titles)
+    for title in titles:
+        titles_array.append(title.text)
+    #print(titles_array)
+
+    # get creators
+    creators = driver.find_elements_by_id("channel-name")
+    for creator in creators:
+        creators_array.append(creator.text)
+    #print(creators_array)
+
+    # get urls
+    urls = driver.find_elements_by_id("video-title-link")
+    for url in urls:
+        urls_array.append(url.get_attribute("href"))
+    #print(urls_array)
+
+    # generate json
+
+    json_string = [{"title": t, "creator": s, "url": x} for t, s, x in zip(titles_array, creators_array, urls_array)]
+        
+    json_to_return = json.dumps(json_string)
+    
+    #driver.close()
+
+    print(json_to_return)
+    return json_to_return
 
 get_main_page()
 
