a47bae6d354c2fb3c2105fd0a28a95910496cb0b
Your Name <legendofmiracles@protonmail.com>
2020-08-11 14:55:07 +0200

    trying to parse youtube homepage. this is starting great already

--- backend.py
+++ backend.py
@@ -1,15 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox()
+
 
 def request(url):
-    response = requests.get(url)
+    response = driver.get(url)
     return response
 
 def get_main_page():
-    html = request('https://www.youtube.com/').content
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
-    titles = soup.find_all("yt-formatted-string")
+    html = request('https://www.youtube.com/')
+    titles = driver.get_element_by_name("a")
+    titles = soup.find_all('a', id='video-title-link')['title']
     print(titles)
 
 get_main_page()
