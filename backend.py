ef0c11abf3f283eb2f0f50ac2e03c475b94fc402
Your Name <legendofmiracles@protonmail.com>
2020-08-12 04:17:44 +0200

    now returns a array of objects, and not anymore json, which is now more maintainable, robust, and smaller codebase

--- backend.py
+++ backend.py
@@ -6,55 +6,52 @@ import json
 driver = webdriver.Firefox()
 
 
+class yt_video:
+    def __init__(self, title_arg, creator_arg, url_arg):
+        #global title
+        #global creator
+        #global url
+        self.title = title_arg
+        self.creator = creator_arg
+        self.url = url_arg
+
 def request(url):
     driver.get(url)
     #return response
 
 def get_main_page():
-    titles_array, urls_array,  creators_array = [], [], []
+    titles_array = []
+    urls_array = []
+    creators_array = []
     request('https://www.youtube.com/')
     
     # get titles
     titles = driver.find_elements_by_id("video-title")
     for title in titles:
             titles_array.append(title.text)
-    #print(titles_array)
+    
 
     # get creators
     creators = driver.find_elements_by_id("channel-name")
     for creator in creators:
         creators_array.append(creator.text)
-    #print(creators_array)
 
     # get urls
     urls = driver.find_elements_by_id("video-title-link")
     for url in urls:
         urls_array.append(url.get_attribute("href"))
-    #print(urls_array)
+    
 
-    # generate json
+    return_value = []
+    for i in range(len(titles_array)):
+        if titles_array[i - 1] != "":
+            return_value.append(yt_video(titles_array[i - 1], creators_array[i - 1], urls_array[i - 1]))
 
-    json_string = [{"title": t, "creator": s, "url": x} for t, s, x in zip(titles_array, creators_array, urls_array)]
-        
-    json_to_return = json.dumps(json_string)
-    
-    #print(json_to_return)
-    #print(len(json_to_return))
-    
-    for i in range(len(json.loads(json_to_return))):
-        
-        if json.loads(json_to_return)[i ]['title'] == "":
-            print(i)
-            
-            
-            print( json.loads(json_to_return)[i])
-    
-    print(json_to_return)
-    
-    return json_to_return
+    return return_value
 
 get_main_page()
 driver.close()
 
 def search(text):
     pass
+
