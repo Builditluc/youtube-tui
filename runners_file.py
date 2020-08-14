6a9ec4f938115dfc3f64c812e27e1fe18c09e806
Your Name <legendofmiracles@protonmail.com>
2020-08-14 11:03:11 +0200

    commented out a unnecessary line

--- runners_file.py
+++ runners_file.py
@@ -36,4 +36,4 @@ def watch_video(url, method: methods):
 
 
 
-watch_video("'https://www.youtube.com/watch?v=dQw4w9WgXcQ'", methods("asdf", "mpv"))
+#watch_video("'https://www.youtube.com/watch?v=dQw4w9WgXcQ'", methods("asdf", "mpv"))
