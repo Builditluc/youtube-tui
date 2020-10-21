7098dc1e1bc064667f81267d26980b01c96e3cf0
builditluc <37375448+Builditluc@users.noreply.github.com>
2020-10-21 17:40:04 +0200

    Prepared the frontend to be implemented

--- src/youtube-tui.py
+++ src/youtube-tui.py
@@ -14,31 +14,25 @@ class Youtube_form(npyscreen.FormBaseNew):
     def create(self):
         super(__class__, self).create()
         self.cycle_widgets = True
-
-        key_handlers = {
-            "^Q" : self.exit_func
-        }
-        self.add_handlers(key_handlers)
+        self.add_handlers({"^Q": lambda _input: exit(0)})
 
         y, x = self.useable_space()
 
         search_bar: Search_widget = self.add(Search_widget, name="Search", rely=1, max_height=3)
-        search_bar.entry_widget.handlers.update({curses.ascii.NL:self.start_search})
+        search_bar.entry_widget.handlers.update({curses.ascii.NL: self.start_search})
 
         video_list: Video_list_widget = self.add(Video_list_widget, name="Home Page", rely=5)
-        video_list.values = ["Test1", "Test2", "Test3"]
-
-    def while_editing(self, widget):
-        pass
+        video_list.entry_widget.handlers.update({curses.ascii.NL: self.select_video})
 
     def afterEditing(self):
         self.parentApp.setNextForm(None)
 
+    def select_video(self, _input):
+        pass
+
     def start_search(self, _input):
         pass
 
-    def exit_func(self, _input):
-        exit(0)
 
 class Application(npyscreen.NPSAppManaged):
     def onStart(self):
