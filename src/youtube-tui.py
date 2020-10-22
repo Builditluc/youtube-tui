e922ab3364c33fb76483580527490fa757ce6378
builditluc <37375448+Builditluc@users.noreply.github.com>
2020-10-22 18:08:28 +0200

    Added the help menu and populated it (yet the bindings are hard-coded)

--- src/youtube-tui.py
+++ src/youtube-tui.py
@@ -20,22 +20,56 @@ class Video_list_widget(npyscreen.BoxTitle):
             self.parent.select_video(self.values[self.value])
 
 
+class Help_info_widget(npyscreen.BoxTitle):
+    _contained_widget = npyscreen.FixedText
+
+
+class Help_grid_widget(npyscreen.SimpleGrid):
+    _contained_widgets = npyscreen.FixedText
+    default_column_number = 2
+
+    def create(self):
+        self.values = [
+            ["Description", "Binding"],
+            ["Cycle through widgets", "Tab"],
+            ["Move up", "Up"],
+            ["Move down", "Down"],
+            ["Open the help menu", "?"],
+            ["Close the help menu", "OK Button"]
+        ]
+
+
+class Help_form(npyscreen.ActionFormMinimal):
+    def create(self):
+        super(__class__, self).create()
+        self.cycle_widgets = True
+
+        self.add(npyscreen.FixedText)
+        grid: Help_grid_widget = self.add(Help_grid_widget, editable=False)
+        grid.create()
+
+    def afterEditing(self):
+        self.parentApp.setNextForm("MAIN")
+
+
 class Youtube_form(npyscreen.FormBaseNew):
     def create(self):
         super(__class__, self).create()
         self.cycle_widgets = True
-        self.add_handlers({"^Q": lambda _input: exit(0)})
+        self.add_handlers({"^Q": lambda _input: exit(0), "?": self.show_help})
 
-        search_bar: Search_widget = self.add(Search_widget, w_id="search_bar", name="Search", rely=1, max_height=3)
+        y, x = self.useable_space()
+
+        search_bar: Search_widget = self.add(Search_widget, w_id="search_bar", name="Search", rely=1, max_width=self.max_x-18, max_height=3)
         search_bar.entry_widget.handlers.update({curses.ascii.NL: self.start_search})
 
-        self.add(Video_list_widget, w_id="video_list", name="Home Page", rely=5, values=[
+        help_text: Help_info_widget = self.add(Help_info_widget, w_id="help_text", name="Help", editable=False, rely=1, relx=self.max_x-16, max_width=13, max_height=3)
+        help_text.value = "Press ?"
+
+        self.add(Video_list_widget, w_id="video_list", name="Home Page", values=[
             YtVideo("Programming Tutorial", "TheCherno", "https://youtube.com/watch="),
             YtVideo("The new MacBook Pro is awesome", "MKBHD", "https://youtube.com/watch=")
         ])
-
-    def afterEditing(self):
-        self.parentApp.setNextForm(None)
 
     def select_video(self, video:str):
         pass
@@ -48,10 +82,14 @@ class Youtube_form(npyscreen.FormBaseNew):
 
         video_widget.display()
 
+    def show_help(self, _input):
+        self.parentApp.switchForm("HELPTEXT")
+
 
 class Application(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm("MAIN", Youtube_form, name="Youtube-TUI")
+        self.addForm("HELPTEXT", Help_form, name="Help")
 
 
 if __name__ == '__main__':
